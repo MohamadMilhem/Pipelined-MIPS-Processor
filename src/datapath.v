993af1bd91d2c43ad358d050287d23976239026b
Tariq Zaghal <1210158@student.birzeit.edu>
2025-01-10 05:52:31 +0200

    create datapath except for hazard unit

--- src/datapath.v
+++ src/datapath.v
@@ -8,34 +8,36 @@
  
  
 module datapath(input  clk, reset,
-				input  [2:0] EXALUOp
+				input  [2:0] EXALUOp,
 				input  [1:0] RegSrc2, PCSrc,
 				input sign_extend_imm, RegSrc1,MEMSignExtendMemData, RegDest,MEMWBdata, EXALUSrc, EXMemDataIn,
-    			input WBRegWr, EXRegWr, MEMRegWr, DMemWr, EXMemRd, EXMemWr, MEMMemRd, MEMMemWr, killF, PCsrcJType, RRSrc, NOOP, CntInst, ALUInst, BRANCH_OR_FOR
+    			input WBRegWr, EXRegWr, MEMRegWr, DMemWr, EXMemRd, EXMemWr, MEMMemRd, MEMMemWr, killF, PCsrcJType, RRSrc, NOOP, CntInst, ALUInst, BRANCH_OR_FOR,
                 output	Z, stall,
                 output [15:0] instrAddrF,
                 input  [15:0] instrF, 
                 output [15:0] DataInM, ALUOutM,
                 input  [15:0] DataOut, 
-                output [3:0]  Opcode);
+                output [3:0]  Opcode,
+				output [2:0] Function
+				);
 										
 	// wires in the fetch stage				
 	wire [15:0] PCPlus1F, branchAddr, selectedInstruction;
-	wire [15:0] untakenBranchPC;
-	wire [15:0] JtypeMuxOutput;
+	//wire [15:0] untakenBranchPC;
+	wire [15:0] JtypeMuxOutput,NoOpMuxOutput;
 	wire [15:0] PCNext;
 	// wires in the decode stage
 	wire [1:0] ForwardBus1, ForwardBus2;
-	wire ForwardME;
+	wire ForwardME, StoreALUOpFw, StoreDataInFw;
 	wire [2:0] RegSrc1D, RegSrc2D, RegDestD;
-	wire [15:0] Bus1D, Bus2D, SubOut, instrAddrD, PCPlus2D;
+	wire [15:0] Bus1D, Bus2D, SubOut, instrAddrD, PCPlus1D, RROutput, BranchOrForMuxOutput;
 	wire [15:0] ForwardedData1, ForwardedData2, extendedImmediateD, extendedImmediateS, instrD;
 	
 	// wires in the execute stage
-	wire EXForwardME;
+	wire EXForwardME,EXStoreALUOpFw, EXStoreDataInFw;
 	wire [2:0] RegDestE;
-	wire [15:0] ALUOutE, ALUIn2, SelectedDataIn, DataInE, extendedImmediateE, instrAddrE,
-	Bus1E, Bus2E, PCPlus2E;
+	wire [15:0] ALUOutE, ALUIn1, ALUIn2, SelectedDataIn, DataInE, extendedImmediateE, instrAddrE,
+	Bus1E, Bus2E, PCPlus1E;
 	
 	// wires in the memory stage
 	
@@ -47,43 +49,48 @@ module datapath(input  clk, reset,
 	wire [2:0] RegDestW;
 	
 	// hazard detection
-	hazard    h(RegDestE, RegDestM, RegSrc1D, RegSrc2D,
+	hazard    h(RegDestE, RegDestM, RegDestW, RegSrc1D, RegSrc2D,
 	EXRegWr, MEMRegWr, EXMemRd, DMemWr,
 	ForwardME,
 	ForwardBus1, ForwardBus2, 
 	stall);
 	
 	// Fetch stage logic
-	mux2 #(16) untakenBranchMUX(PCPlus2F, instrAddrF, killF, untakenBranchPC);
-	mux2 #(16) JTypeMux({instrAddrF[15:9], instrD[11:3]}, instrAddrF, killF, untakenBranchPC);
-	mux4 PCinput(untakenBranchPC, branchAddr, {instrAddrF[15:13], instrD[11:0], 1'b0}, Bus1D, PCSrc, PCNext);
+	mux2 #(16) NoOpMux(PCPlus1D, PCNext, killF, NoOpMuxOutput);	 
+	mux2 #(16) JTypeMux({instrAddrF[15:9], instrD[11:3]}, RROutput, PCsrcJType, JtypeMuxOutput);
+	mux4 PCinput(NoOpMuxOutput,branchAddr, Bus2D, JtypeMuxOutput , PCSrc, PCNext);	 
 	flopenr #(16) PC(clk, reset, ~stall, PCNext, instrAddrF);	// PC register
-	adder       pcadd1(instrAddrF, 16'b10, PCPlus2F);	// adder to increment PC address
-	mux2 #(16) fetchedInstructionSelector(instrF, 16'b0000000000000000, killF, selectedInstruction);
+	adder       pcadd1(instrAddrF, 16'b0000000000000001, PCPlus1F); // adder to increment PC address
+	mux2 #(16) fetchedInstructionSelector(instrF, 16'b0000000000000000, killF, selectedInstruction); //	instrF is the output of the IMEM
+
 	
 	// IF/ID Buffers
 	flopenr #(16) PCBufferF(clk, reset, ~stall, instrAddrF, instrAddrD);	// PC Buffer
-	flopenr #(16) PCPlusBufferF(clk, reset, ~stall, PCPlus2F, PCPlus2D);	// PC+2 Buffer
+	flopenr #(16) PCPlusBufferF(clk, reset, ~stall, PCPlus1F, PCPlus1D);	// PC+2 Buffer
 	flopenr #(16) FetchedInstrBufferF(clk, reset, ~stall, selectedInstruction, instrD);	// Fetched Instruction Buffer
 	
 	// Decode stage logic
 	assign Opcode = instrD[15:12];
-	assign mode = instrD[11];
-	mux4 #(3) regSrc1Selector(instrD[8:6], instrD[7:5], 3'b111, 3'b000, RegSrc1, RegSrc1D);			
-	mux4 #(3) regSrc2Selector(instrD[5:3], instrD[10:8], instrD[10:8], instrD[11:9], RegSrc2, RegSrc2D);
-	mux4 #(3) regDestSelector(instrD[11:9], instrD[10:8], 3'b111, 3'b000, RegDest, RegDestD);
-	regfile rf(clk, WBRegWr, RegSrc1D, RegSrc2D, RegDestW, WrittenDataW, Bus1D, Bus2D);
+	assign Function = instrD[2:0];
+	mux2 #(3) regSrc1Selector(instrD[8:6], instrD[11:9], RegSrc1, RegSrc1D);			
+	mux4 #(3) regSrc2Selector(instrD[5:3], instrD[8:6], instrD[11:9], RegSrc2, RegSrc2D);
+	mux2 #(3) regDestSelector(instrD[11:9], instrD[8:6], RegDest, RegDestD);  
+	
+	regfile rf(clk, WBRegWr, RegSrc1D, RegSrc2D, RegDestW, WrittenDataW, Bus1D, Bus2D);	
+	
 	mux4 #(16) ForwardBus1Selector(Bus1D, ALUOutE, WrittenDataM, WrittenDataW, ForwardBus1, ForwardedData1);
 	mux4 #(16) ForwardBus2Selector(Bus2D, ALUOutE, WrittenDataM, WrittenDataW, ForwardBus2, ForwardedData2);
 	
 	
 	
-	signext #(5) itype_extender(instrD[4:0], sign_extend_imm, extendedImmediateD);
-	
-	adder branchAdder(extendedImmediateD, instrAddrD, branchAddr);
-	subtractor sb(ForwardedData1, ForwardedData2, Z, N, V, SubOut);
+	signext #(6) itype_extender(instrD[5:0], sign_extend_imm, extendedImmediateD);
 	
 	
+	adder branchAdder(extendedImmediateD, instrAddrD, branchAddr);	
+	
+	mux2 #(16) BranchOrForMux(ForwardedData2, 16'b0000000000000001, BRANCH_OR_FOR, BranchOrForMuxOutput);
+	
+	subtractor sb(ForwardedData1, BranchOrForMuxOutput, Z, SubOut);
 	
 	// ID/EX Buffers
 	flopr #(16) ImmBufferE(clk, reset, extendedImmediateD, extendedImmediateE);
@@ -91,34 +98,26 @@ module datapath(input  clk, reset,
 	flopr #(16) Bus1BufferE(clk, reset, ForwardedData1, Bus1E);
 	flopr #(16) Bus2BufferE(clk, reset, ForwardedData2, Bus2E);
 	flopr #(3) RdE(clk, reset, RegDestD, RegDestE);
-	flopr #(16) PCPlusBufferE(clk, reset, PCPlus2D, PCPlus2E);
-	flopr #(16) ExtendedSTypeImmediate(clk, reset, {{(7){instrD[8]}} ,instrD[8:0]}, extendedImmediateS);
-	flopr #(1)  ForwardMEBuffer(clk, reset, ForwardME, EXForwardME);
-	
+	flopr #(16) PCPlusBufferE(clk, reset, PCPlus1D, PCPlus1E);
+	flopr #(1)  StoreDataInFwBuffer(clk, reset, StoreDataInFw, EXStoreDataInFw);
+	flopr #(1)  StoreALUOpFwBuffer(clk, reset, StoreALUOpFw, EXStoreALUOpFw);
 	
 	// Execute Stage Logic
-	mux2 #(16) ALUInputSelector(Bus2E, extendedImmediateE, EXALUSrc, ALUIn2);
+	mux2 #(16) ALUInput1Selector(Bus1E, DataOut, EXStoreALUOpFw, ALUIn1); 
+	mux2 #(16) ALUInput2Selector(Bus2E, extendedImmediateE, EXALUSrc, ALUIn2);
+	alu ALU(ALUIn1, ALUIn2, EXALUOp, ALUOutE);
+	mux2 #(16) DataInSelector(Bus2E, DataOut, EXStoreDataInFw, DataInE); 
 	
-	alu ALU(Bus1E, ALUIn2, EXALUOp, ALUOutE);
-	
-	
-	
-	
-	mux2 #(16) ForwardedMemDataSelector(Bus2E, DataOut, EXForwardME, DataInE);
-	mux2 #(16) DataInSelector(DataInE, extendedImmediateS, EXMemDataIn, SelectedDataIn);
-	
-	// EX/MEM Buffers
-	
+	// EX/MEM Buffers  
 	flopr #(16) ALUOutBuffer(clk, reset, ALUOutE, ALUOutM);
-	flopr #(16)	DataInBuffer(clk, reset, SelectedDataIn, DataInM);
-	flopr #(16)	NextPCBuffer(clk, reset, PCPlus2E, PCPlus2M);
+	flopr #(16)	DataInBuffer(clk, reset, DataInE, DataInM);
 	flopr #(3)	RdMBuffer(clk, reset, RegDestE, RegDestM);
 	
 	
 	// Memory Stage Logic
 	
 	signext #(8) MemDataExtender(DataOut[7:0], MEMSignExtendMemData, extendedDataOut);
-	mux4 #(16) WBDataSelector(ALUOutM, DataOut, extendedDataOut, PCPlus2M, MEMWBdata, WrittenDataM);
+	mux4 #(16) WBDataSelector(ALUOutM, DataOut,  MEMWBdata, WrittenDataM);
 	
 	// MEM/WB Buffers
 	
